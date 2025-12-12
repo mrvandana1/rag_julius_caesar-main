d680d7115b1f2123b5215c7de51a605150c4bde9
mrvandana1 <vandanamohanaraj@gmail.com>
2025-12-12 22:01:27 +0530

    doen

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,7 +3,7 @@ import requests
 
 st.set_page_config(page_title="Shakespearean Scholar", layout="wide")
 
-st.title(" dd Shakespearean Scholar – Julius Caesar RAG System")
+st.title(" diddy Shakespearean Scholar – Julius Caesar RAG System")
 st.write("Ask me bro any question about *Julius Caesar* and get a scholarly answer with citations.")
 ##
 # API_URL = "http://localhost:8000/query"
