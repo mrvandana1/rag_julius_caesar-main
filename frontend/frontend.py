ff575335219f77b90f079181ffac279fd8016fa0
mrvandana1 <vandanamohanaraj@gmail.com>
2025-12-12 21:44:54 +0530

    ansible21122

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -3,7 +3,7 @@ import requests
 
 st.set_page_config(page_title="Shakespearean Scholar", layout="wide")
 
-st.title(" DEMN22 Shakespearean Scholar – Julius Caesar RAG System")
+st.title(" DEMN222222222 Shakespearean Scholar – Julius Caesar RAG System")
 st.write("Ask me bro any question about *Julius Caesar* and get a scholarly answer with citations.")
 ##
 # API_URL = "http://localhost:8000/query"
